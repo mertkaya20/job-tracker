577f681f18297dd0194fe0538d058c63c4eb19ee
mertkaya20 <mertkaya.codes@gmail.com>
2026-07-10 13:25:48 +0300

    feat: add production env config and axios base url

--- client/src/api/axios-instance.js
+++ client/src/api/axios-instance.js
@@ -3,7 +3,7 @@ import store from "../store/index";
 import { logout } from "../store/auth-slice";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
